ec95079c89814468241ec5bcef99794359121daa
Pierre-Emmanuel DENYS <pedenys@pm.me>
2020-10-31 15:15:12 +0100

    Update twitter link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,33 +1,37 @@
-import Link from 'next/link'
-import styled from 'styled-components'
+import Link from "next/link";
+import styled from "styled-components";
 
 type FooterProps = {
-    year: string
-}
+  year: string;
+};
 
 const FooterContainer = styled.div`
-    display:flex;
-    flex-direction: row;
-    width: 100%;
-    justify-content:center;
-    a {
-        margin-right:2rem;
-    }
-    a:last-of-type {
-        margin-right: 0rem;
-    }
-`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
+  a {
+    margin-right: 2rem;
+  }
+  a:last-of-type {
+    margin-right: 0rem;
+  }
+`;
 
 const Footer: React.FunctionComponent<FooterProps> = ({ year }) => {
-    return (
-        <FooterContainer>
-            <footer>
-                <Link href="/licence"><a>© {year} CC-BY-SA 4.0</a></Link>
-                <Link href="/about"><a>À propos</a></Link>
-                <a href="https://twitter.com/apolineaire">🐦</a>
-            </footer>
-        </FooterContainer>
-    )
-}
+  return (
+    <FooterContainer>
+      <footer>
+        <Link href="/licence">
+          <a>© {year} CC-BY-SA 4.0</a>
+        </Link>
+        <Link href="/about">
+          <a>À propos</a>
+        </Link>
+        <a href="https://twitter.com/_pedenys_">🐦</a>
+      </footer>
+    </FooterContainer>
+  );
+};
 
-export default Footer
+export default Footer;
