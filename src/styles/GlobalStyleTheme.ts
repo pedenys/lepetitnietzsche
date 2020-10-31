07f2b658c0659b45a8dec4a572130ab42dfb4f1b
Pierre-Emmanuel DENYS <pedenys@pm.me>
2020-10-31 16:04:56 +0100

    Remove useless fonts and fonts declaration

--- src/styles/GlobalStyleTheme.ts
+++ src/styles/GlobalStyleTheme.ts
@@ -1,75 +1,66 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyleTHeme = createGlobalStyle`
     /* Fonts declaration */
     @font-face {
     font-family: 'Space Mono';
-    src: url('/static/fonts/SpaceMono-BoldItalic.woff2') format('woff2'),
-        url('/static/fonts/SpaceMono-BoldItalic.woff') format('woff');
+    src: url('/static/fonts/SpaceMono-BoldItalic.woff') format('woff');
     font-weight: bold;
     font-style: italic;
     }
 
     @font-face {
         font-family: 'Space Mono';
-        src: url('/static/fonts/SpaceMono-Italic.woff2') format('woff2'),
-            url('/static/fonts/SpaceMono-Italic.woff') format('woff');
+        src: url('/static/fonts/SpaceMono-Italic.woff') format('woff');
         font-weight: normal;
         font-style: italic;
     }
 
     @font-face {
         font-family: 'Eczar';
-        src: url('/static/fonts/Eczar-Bold.woff2') format('woff2'),
-            url('/static/fonts/Eczar-Bold.woff') format('woff');
+        src: url('/static/fonts/Eczar-Bold.woff') format('woff');
         font-weight: bold;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Eczar';
-        src: url('/static/fonts/Eczar-Medium.woff2') format('woff2'),
-            url('/static/fonts/Eczar-Medium.woff') format('woff');
+        src: url('/static/fonts/Eczar-Medium.woff') format('woff');
         font-weight: 500;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Eczar';
-        src: url('/static/fonts/Eczar-Regular.woff2') format('woff2'),
-            url('/static/fonts/Eczar-Regular.woff') format('woff');
+        src: url('/static/fonts/Eczar-Regular.woff') format('woff');
         font-weight: normal;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Eczar';
-        src: url('/static/fonts/Eczar-ExtraBold.woff2') format('woff2'),
-            url('/static/fonts/Eczar-ExtraBold.woff') format('woff');
+        src: url('/static/fonts/Eczar-ExtraBold.woff') format('woff');
         font-weight: 800;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Space Mono';
-        src: url('/static/fonts/SpaceMono-Bold.woff2') format('woff2'),
-            url('/static/fonts/SpaceMono-Bold.woff') format('woff');
+        src: url('/static/fonts/SpaceMono-Bold.woff') format('woff');
         font-weight: bold;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Space Mono';
-        src: url('/static/fonts/SpaceMono-Regular.woff2') format('woff2'),
-            url('/static/fonts/SpaceMono-Regular.woff') format('woff');
+        src:url('/static/fonts/SpaceMono-Regular.woff') format('woff');
         font-weight: normal;
         font-style: normal;
     }
 
     @font-face {
         font-family: 'Eczar';
-        src: url('/static/fonts/Eczar-SemiBold.woff2') format('woff2'),
-            url('/static/fonts/Eczar-SemiBold.woff') format('woff');
+        src:url('/static/fonts/Eczar-SemiBold.woff') format('woff');
         font-weight: 600;
         font-style: normal;
     }
@@ -88,17 +79,17 @@ const GlobalStyleTHeme = createGlobalStyle`
         font-family:'Eczar';
         font-size:1.8rem;
         line-height:2;
-        color: ${props => props.theme.colors.black};
+        color: ${(props) => props.theme.colors.black};
     }
     a {
         text-decoration:none;
         color: '#000'
     }
     a:hover{
-        color:${props => props.theme.colors.black} !important;
+        color:${(props) => props.theme.colors.black} !important;
     }
     a:visited{
-        color: ${props => props.theme.colors.lightblue}     ;
+        color: ${(props) => props.theme.colors.lightblue}     ;
     }
 
     @keyframes fadeIn {
@@ -118,6 +109,6 @@ const GlobalStyleTHeme = createGlobalStyle`
             opacity: 0;
         }
     }
-`
+`;
 
-export default GlobalStyleTHeme
+export default GlobalStyleTHeme;
